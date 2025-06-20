cd017a078c91f9fa7ef80dd80f6061642a05fb64
Viktoriia Popel <xructa2009@gmail.com>
2025-06-20 21:59:39 +0100

    fix task 1

--- js/task-1.js
+++ js/task-1.js
@@ -1,45 +1,27 @@
 
-function calcAverageCalories(days) {
-  if (days.length === 0) {
-    return 0;
+
+function isEnoughCapacity(products, containerSize) {
+  let totalProducts = 0;
+
+  for (const product in products) {
+    totalProducts += products[product];
   }
 
-  let totalCalories = 0;
-
-  for (const day of days) {
-    totalCalories += day.calories;
-  }
-
-  const averageCalories = totalCalories / days.length;
-  return Math.round(averageCalories); 
+  return totalProducts <= containerSize;
 }
 
-
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
 
 console.log(
-  calcAverageCalories([
-    { day: "monday", calories: 3010 },
-    { day: "tuesday", calories: 3200 },
-    { day: "wednesday", calories: 3120 },
-    { day: "thursday", calories: 2900 },
-    { day: "friday", calories: 3450 },
-    { day: "saturday", calories: 3280 },
-    { day: "sunday", calories: 3300 }
-  ])
-); // 3180
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
 
 console.log(
-  calcAverageCalories([
-    { day: "monday", calories: 2040 },
-    { day: "tuesday", calories: 2270 },
-    { day: "wednesday", calories: 2420 },
-    { day: "thursday", calories: 1900 },
-    { day: "friday", calories: 2370 },
-    { day: "saturday", calories: 2280 },
-    { day: "sunday", calories: 2610 }
-  ])
-); // 2270
+  isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
+); // true
 
 console.log(
-  calcAverageCalories([])
-); // 0
+  isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
+); // false
